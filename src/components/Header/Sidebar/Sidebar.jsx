a2380e630f320abd29b42c58916b5d2472a91c46
renatormsilva <renato.brtkm@gmail.com>
2022-08-22 00:06:43 -0400

    refactor: link names in sidebar

--- src/components/Header/Sidebar/Sidebar.jsx
+++ src/components/Header/Sidebar/Sidebar.jsx
@@ -51,7 +51,7 @@ export default function Sidebar(props) {
       backgroundColor="primary.light"
     >
       <List>
-        {["Home", "Quotes", "Leads", "Tours"].map((text, index) => (
+        {["home", "quotes", "leads", "tours"].map((text, index) => (
           <ListItem key={text} sx={{ padding: ["15px", "20px"] }}>
             <ListItemButton
               component={Link}
